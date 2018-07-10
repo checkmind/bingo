b3b0693ae05d8a0bf9743beb326d153a8a32e6a7
checkmind <1036971959@qq.com>
2018-07-11 00:00:14 +0800

    增加微信配置文件

---
+++ wxFile/platform.js
@@ -0,0 +1,82 @@
+/**
+ * 请在白鹭引擎的Main.ts中调用 platform.login() 方法调用至此处。
+ */
+
+class WxgamePlatform {
+
+    name = 'wxgame'
+
+    login() {
+        return new Promise((resolve, reject) => {
+            wx.login({
+                success: (res) => {
+                    resolve(res)
+                }
+            })
+        })
+    }
+
+    getUserInfo() {
+        return new Promise((resolve, reject) => {
+            wx.getUserInfo({
+                withCredentials: true,
+                success: function (res) {
+                    var userInfo = res.userInfo
+                    var nickName = userInfo.nickName
+                    var avatarUrl = userInfo.avatarUrl
+                    var gender = userInfo.gender //性别 0：未知、1：男、2：女
+                    var province = userInfo.province
+                    var city = userInfo.city
+                    var country = userInfo.country
+                    resolve(userInfo);
+                }
+            })
+        })
+    }
+    createInnerAudioContext () {
+        let music =  wx.createInnerAudioContext()
+        music.src = 'http://cangnanshi.com/bingo/music.mp3'
+        //music.play();
+    }
+    playClearMusic() {
+        let music =  wx.createInnerAudioContext()
+        music.src = 'http://cangnanshi.com/bingo/clear.mp3'
+        //music.play();
+    }
+    playButtonMusic() {
+        let music =  wx.createInnerAudioContext()
+        music.src = 'http://cangnanshi.com/bingo/button.mp3'
+        //music.play();
+    }
+
+    openDataContext = new WxgameOpenDataContext();
+}
+
+class WxgameOpenDataContext {
+
+    createDisplayObject(type,width,height){
+        const bitmapdata = new egret.BitmapData(sharedCanvas);
+        bitmapdata.$deleteSource = false;
+        const texture = new egret.Texture();
+        texture._setBitmapData(bitmapdata);
+        const bitmap = new egret.Bitmap(texture);
+        bitmap.width = width;
+        bitmap.height = height;
+
+        egret.startTick((timeStarmp) => {
+            egret.WebGLUtils.deleteWebGLTexture(bitmapdata.webGLTexture);
+            bitmapdata.webGLTexture = null;
+            return false;
+        }, this);
+        return bitmap;
+    }
+
+
+    postMessage(data){
+        const openDataContext = wx.getOpenDataContext();
+        openDataContext.postMessage(data);
+    }
+}
+
+
+window.platform = new WxgamePlatform();
